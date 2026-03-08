75745ba01c266bfe1859b160507c5ce51f60847c
Félix Piédallu <felix@piedallu.me>
2026-03-08 18:51:11 +0100

    Use ntfables config instead of iptables script

--- conf/iptables.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-iptables -t nat -A PREROUTING -i __IFACE__ -p tcp --dport 53 -j REDIRECT --to-port __PORT_NAMED__
-iptables -t nat -A PREROUTING -i __IFACE__ -p udp --dport 53 -j REDIRECT --to-port __PORT_NAMED__
-
-ip6tables -t nat -A PREROUTING -i __IFACE__ -p tcp --dport 53 -j REDIRECT --to-port __PORT_NAMED__
-ip6tables -t nat -A PREROUTING -i __IFACE__ -p udp --dport 53 -j REDIRECT --to-port __PORT_NAMED__
